28f8205bce69ad1ec83032a8df074c25bb6c696d
bassanipedro <pedrohenriquebassani03@gmail.com>
2024-10-21 20:56:42 -0300

    fixed CORS errors

--- ia/app.py
+++ ia/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from model import CreditRiskModel
 
 app = Flask(__name__)
+CORS(app) 
 
 model = CreditRiskModel('german_credit_data.csv')
 model.train()
